031ae2c73e98ceda46d104af2bdb85953fcdc7f0
Matei1199 <mateiibln@gmail.com>
2025-09-09 01:36:12 +0300

    Inceput de pagina(Header si Footer)

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import Image from "next/image";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function Home() {
+
   return (
       <div>
-        <h1>Prima parte</h1>
+          <Header />
+          <main className="p-4">
+              <h1>Prima parte</h1>
+              <p className="text-lg mt-4">
+                  Cel mai bun site pentru a fi la curent cu ultimele știri și informații din lumea Binance și a criptomonedelor!
+              </p>
+          </main>
+          <Footer />
       </div>
   );
 }
