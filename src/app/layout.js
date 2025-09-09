c3310af3285db496ba6f74df82d1495b59401420
Matei1199 <mateiibln@gmail.com>
2025-09-09 12:06:47 +0300

    Creat cateva clase

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className= "min-h-screen flex flex-col"
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
       <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow p-4">
-              <h1 className ="text-center">Prima parte</h1>
-              <p className="text-lg mt-4">
+              <h1 className ="mainTitle">Prima parte</h1>
+              <p className="paragraph1">
                   Cel mai bun site pentru a fi la curent cu ultimele știri și informații din lumea Binance și a criptomonedelor!
               </p>
           </main>
